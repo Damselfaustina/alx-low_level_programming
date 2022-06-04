d1d0a6eaa9c2a826726204dc3b0aa10fce12589f
Damselfaustina <joyeneonuh@yahoo.com>
2022-06-03 23:01:10 -0700

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return; 0 (Success)
+ */
+int main(void
+{
+	char c = 'a';
+	int i = 0;
+	
+	while (i < 26)
+	{
+		putchar(c + i);)
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
