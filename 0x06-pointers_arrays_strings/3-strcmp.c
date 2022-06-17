f1ad57e40f3f0defd8003e69cfa29bd8dfe2697d
Damselfaustina <joyeneonuh@yahoo.com>
2022-06-16 20:10:39 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,19 @@
-  #include "main.h"
-  #include <string.h>
+ #include "main.h"
+ #include <string.h>
 
-  /**
-   * _strncat -  a function that concatenates two strings.
-   * @src: first parameter.
-   * @dest: second parameter.
-   * @n: third parameter.
-   * Return: 0
-   */
+ /**
+  * _strncat -  a function that concatenates two strings.
+  * @src: first parameter.
+  * @dest: second parameter.
+  * @n: third parameter.
+  * Return: 0
+  */
 
-  char *_strncat(char *dest, char *src, int n)
-  {
-  <<<<<<< HEAD
-	  return (strncat(dest, src, n));
-  =======
-	  return (strcmp(s1, s2));
-  >>>>>>> 567c778ab44697a7160d3032cf16a16d41440820
-  }
+ char *_strncat(char *dest, char *src, int n)
+ {
+ <<<<<<< HEAD
+	 return (strncat(dest, src, n));
+ =======
+	 return (strcmp(s1, s2));
+ >>>>>>> 567c778ab44697a7160d3032cf16a16d41440820
+ }
