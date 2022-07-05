d921460c81112d3f9f0899f08655bcb8fa42ea4d
Damselfaustina <joyeneonuh@yahoo.com>
2022-07-05 14:43:44 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+}
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -28,3 +28,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
+
